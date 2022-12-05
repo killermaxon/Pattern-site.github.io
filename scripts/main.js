2c5e2b499e24dae4276c5aec3c0df7888bb58492
killermaxon <98490924+killermaxon@users.noreply.github.com>
2022-12-05 22:51:16 +0300

    1
    1

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+var myImage = document.querySelector('img');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Welcome you, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Welcome you, ' + storedName;
+}
+
+myButton.onclick = function () {
+	setUserName();
+}
+
+myImage.onclick = function () {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/oranges.jpg') {
+		myImage.setAttribute('src', 'images/google-backgraund.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/oranges.jpg');
+	}
+}
